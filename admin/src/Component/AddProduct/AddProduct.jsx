f72ed59eef37cdc4bc514b3d421cd0aa8dfa729a
karmyogiaditya <138021554+adi-singh25@users.noreply.github.com>
2025-05-12 11:45:24 +0530

    Update AddProduct.jsx

--- admin/src/Component/AddProduct/AddProduct.jsx
+++ admin/src/Component/AddProduct/AddProduct.jsx
@@ -36,7 +36,7 @@ const AddProduct = () => {
         let formData=new FormData();
         formData.append('product',image);
 
-        await fetch('http://localhost:4000/upload',{
+        await fetch('https://e-commerce-mernappbackend-1.onrender.com/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
@@ -50,7 +50,7 @@ const AddProduct = () => {
         if(responseData.success){
             product.image=responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://e-commerce-mernappbackend-1.onrender.com/addproduct',{
                 method:'Post',
                 headers:{
                     Accept:'application/json',
